eab11cc407cf043095174c8b400a38e272e305fb
electrolyte-orb <electrolyte.spdf@outlook.com>
2022-06-18 16:14:50 +0000

    fix build.js

--- build.js
+++ build.js
@@ -1,23 +1,25 @@
+import { copyFile } from 'fs';
 import fs from 'fs/promises';
 
 const EXCLUDED_FILES = ['.git', 'dist', 'build.js'];
-function deleteFiles() {
-   const all_files = await fs.readdir('.');
+async function deleteFiles() {
+	const all_files = await fs.readdir('.');
 
-   all_files.forEach((file) => {
-      if (!EXCLUDED_FILES.includes(file)) {
-         fs.rm(file, { recursive: true, force: true });
-      }
-   });
+	all_files.forEach((file) => {
+		if (!EXCLUDED_FILES.includes(file)) {
+			fs.rm(file, { recursive: true, force: true });
+		}
+	});
 }
 
-function moveFiles() {
-   const distFiles = await fs.readdir('dist')
+async function moveFiles() {
+	const distFiles = await fs.readdir('dist');
 
-   distFiles.forEach(file => {
-      fs.cp(`./dist/${file}`, './', { force: true, recursive: true }).then(() => {
-         fs.rm(`./dist/${file}`, { recursive: true, force: true });
-      });
-   })
+	distFiles.forEach((file) => {
+		fs.cp(`./dist/${file}`, './', { force: true, recursive: true }).then(() => {
+			fs.rm(`./dist/${file}`, { recursive: true, force: true });
+		});
+	});
 }
 
+deleteFiles().then(moveFiles);
