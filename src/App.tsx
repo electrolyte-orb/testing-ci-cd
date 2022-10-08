61e6dae35fa8ef24a6fb55082ce682505fd8b26b
electrolyte-orb <electrolyte.spdf@outlook.com>
2022-10-08 05:06:31 +0000

    valhalla update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from '@components';
+import { useState } from 'react';
 
 function App() {
+	const [name, setName] = useState("")
 	return (
 		<>
 			<Navbar />
 			<div className="p-5 text-xl">
-				<h1>Hello worlda</h1>
-				<p>Hello world a</p>
+				<input className='border border-gray-400 px-4 py-1 rounded-lg' type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+				<div>Hello {name}</div>
 			</div>
 		</>
 	);
