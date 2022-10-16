4ac2a4553c1faefcb23eebfc4904128e42e0cd7e
electrolyte-orb <55880176+electrolyte-orb@users.noreply.github.com>
2022-10-16 21:39:28 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="p-5 bg-white sticky top-0 shadow-2xl">
-      <span className="text-blue-600 font-bold">Testing CI/CD</span>
+      <span className="text-blue-600 font-bold">Testing CI/CD Circle CI</span>
     </div>
   );
 }
